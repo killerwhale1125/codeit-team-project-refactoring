062d039fd92babb18d934103187789c8363a4618
killerwhale1125 <killerwhale1125@naver.com>
2024-11-22 14:20:28 +0900

    chore : CICD Script 파일 Docker image name 수정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,5 +4,5 @@ echo "--------------- start deploy -----------------"
 docker stop dev-codeit-server || true
 docker rm dev-codeit-server || true
 docker pull 535002866978.dkr.ecr.ap-northeast-2.amazonaws.com/dev-codeit-server:latest
-docker run -d --name instagram-server -p 8080:8080 535002866978.dkr.ecr.ap-northeast-2.amazonaws.com/dev-codeit-server:latest
-echo "--------------- end deploy -----------------""
+docker run -d --name dev-codeit-server -p 8080:8080 535002866978.dkr.ecr.ap-northeast-2.amazonaws.com/dev-codeit-server:latest
+echo "--------------- end deploy -----------------"
