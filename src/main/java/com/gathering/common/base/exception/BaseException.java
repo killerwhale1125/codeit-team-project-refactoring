31e2928ec029be480f56fba5f7e3477d27087a90
killerwhale1125 <killerwhale1125@naver.com>
2024-11-26 12:43:26 +0900

    feat : 공통 예외 처리 클래스 추가

---
+++ src/main/java/com/gathering/common/base/exception/BaseException.java
@@ -0,0 +1,13 @@
+package com.gathering.common.base.exception;
+
+import com.gathering.common.base.response.BaseResponseStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends RuntimeException {
+    private BaseResponseStatus status;
+}
