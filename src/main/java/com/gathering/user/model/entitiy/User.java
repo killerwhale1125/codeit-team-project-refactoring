70d27b9aed6641368bc6328912fa07cedd09ef40
killerwhale1125 <killerwhale1125@naver.com>
2024-11-28 19:03:23 +0900

    feat : User Entity 관계 추가

--- src/main/java/com/gathering/user/model/entitiy/User.java
+++ src/main/java/com/gathering/user/model/entitiy/User.java
@@ -1,8 +1,12 @@
 package com.gathering.user.model.entitiy;
 
+import com.gathering.challenge.model.entity.ChallengeUser;
 import com.gathering.common.base.jpa.BaseTimeEntity;
+import com.gathering.gathering.model.entity.GatheringUser;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -10,10 +14,13 @@ import java.util.List;
 
 @Entity
 @Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "users_id")
     private long id;
     private String userId;
     private String email;
@@ -24,6 +31,12 @@ public class User extends BaseTimeEntity {
     private Address address;
     private String roles; // USER, ADMIN
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<GatheringUser> gatheringUsers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<ChallengeUser> challengeUsers = new ArrayList<>();
+
     public List<String> getRoleList() {
         if(this.roles.length() > 0) {
             return Arrays.asList(this.roles.split(","));
@@ -32,4 +45,13 @@ public class User extends BaseTimeEntity {
         return new ArrayList<>();
     }
 
+    public void addGatheringUser(GatheringUser gatheringUser) {
+        gatheringUsers.add(gatheringUser);
+        gatheringUser.addUser(this); // 관계 설정
+    }
+
+    public void addChallengeUser(ChallengeUser challengeUser) {
+        challengeUsers.add(challengeUser);
+        challengeUser.addUser(this); // 관계 설정
+    }
 }
