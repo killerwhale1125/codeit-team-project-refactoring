e521519c2ef6ae0b80dc9c94435e70c3a3e9daf9
colin.h.jeong <alfhaos@naver.com>
2024-12-05 19:51:52 +0900

    feat: 리소스 핸들러 추가 (사용자 프로필)

---
+++ src/main/java/com/gathering/common/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.gathering.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/profile/**")
+                .addResourceLocations("file:./file/user/profile/");
+    }
+}
