854eb84cd66d419ab8059e1a7c0f63c9d79798b5
alfhaos <alfhaos@naver.com>
2024-12-07 17:42:16 +0900

    test: docker 볼륨 설정

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -10,6 +10,7 @@ docker pull 535002866978.dkr.ecr.ap-northeast-2.amazonaws.com/dev-codeit-server:
 docker run -d \
   --name dev-codeit-server \
   -p 8080:8080 \
+  -v /file:/file
   --network dev_codeit_network \
   535002866978.dkr.ecr.ap-northeast-2.amazonaws.com/dev-codeit-server:latest
 
