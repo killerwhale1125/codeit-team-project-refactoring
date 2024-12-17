990c261af138c81fb99e988f9d3eaa1b6017f3f9
killerwhale1125 <killerwhale1125@naver.com>
2024-12-17 19:22:16 +0900

    feat : 목표 시간 순서 변경

--- src/main/java/com/gathering/gathering/model/entity/ReadingTimeGoal.java
+++ src/main/java/com/gathering/gathering/model/entity/ReadingTimeGoal.java
@@ -1,10 +1,12 @@
 package com.gathering.gathering.model.entity;
 
 public enum ReadingTimeGoal {
-    TEN_MINUTES(10), // 10분
-    THIRTY_MINUTES(30), // 30분
     ONE_HOUR(60), // 1시간
-    OVER_ONE_HOUR(Integer.MAX_VALUE); // 1시간 이상 (최대값으로 처리)
+    OVER_ONE_HOUR(Integer.MAX_VALUE), // 1시간 이상 (최대값으로 처리)
+    TEN_MINUTES(10), // 10분
+    THIRTY_MINUTES(30); // 30분
+
+
 
     private final int minutes;
 
